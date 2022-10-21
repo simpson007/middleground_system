1872d3a9cc92b9ec93c8dfcacc32932780caa237
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2022-10-21 15:33:01 +0800

    项目初始化

---
+++ src/utils/request.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+const service = axios.create() // 创建axios实例
+service.interceptors.request.use() // 请求拦截器
+service.interceptors.response.use() // 响应拦截器
+export default service // 导出axios实例
