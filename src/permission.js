19b9a0a20b1f556c66ad7ac9da6476501d24a452
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2022-10-27 21:12:09 +0800

    feat: 主页的token拦截处理

---
+++ src/permission.js
@@ -0,0 +1,35 @@
+import router from '@/router'
+import store from '@/store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+const whiteList = ['/login', '/404']
+
+// 路由的前置守卫
+router.beforeEach((to, from, next) => {
+  // 开启进度条
+  NProgress.start()
+  //  首先判断有无token
+  if (store.getters.token) {
+    //   如果有token 继续判断是不是去登录页
+    if (to.path === '/login') {
+      //  表示去的是登录页
+      next('/') // 跳到主页
+    } else {
+      next() // 直接放行
+    }
+  } else {
+    // 如果没有token
+    if (whiteList.indexOf(to.path) > -1) {
+      // 如果找到了 表示在在名单里面
+      next()
+    } else {
+      next('/login') // 跳到登录页
+    }
+  }
+  NProgress.done() // 手动强制关闭一次
+})
+
+router.afterEach(() => {
+  NProgress.done() // 关闭进度条
+})
