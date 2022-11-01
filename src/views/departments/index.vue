166697507a5a3bb50341128ae17b2f4adf6403cb
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2022-11-01 19:02:46 +0800

    feat: 实现组织架构的头部内容

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+        <el-row type="flex" justify="space-between" align="center" style="height: 40px">
+          <el-col>
+            <span>江苏天泽科技有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>
+                负责人
+              </el-col>
+              <el-col>
+                <el-dropdown>
+                  <span>操作<i class="el-icon-arrow-down" /></span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>
+                      请添加子部门
+                    </el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
@@ -14,6 +35,9 @@ export default {
 }
 </script>
 
-  <style>
-
+  <style scoped>
+    .tree-card{
+      padding: 30px 140px;
+      font-size: 14px;
+    }
   </style>
