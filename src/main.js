6d1c9e1bc495ecaa4e254bcabc29b218434b3f00
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2022-11-09 15:30:18 +0800

    feat: 封装一个通用的工具栏

--- src/main.js
+++ src/main.js
@@ -16,7 +16,8 @@ import * as directives from '@/directives'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-
+import Component from '@/components/pageTools'
+ 
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -34,6 +35,8 @@ import '@/permission' // permission control
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+
+Vue.use(Component)
 
 // 遍历所有的导出的指令对象 完成自定义全局注册
 Object.keys(directives).forEach(key => {
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <page-tools :show-before="true">
+        <template v-slot:before>
+          <span>共50条记录</span>
+        </template>
+        <template v-slot:after>
+          <el-button size="small" type="danger">普通excel导出</el-button>
+          <el-button size="small" type="info">复杂表头excel导出</el-button>
+          <el-button size="small" type="success">excel导入</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </page-tools>
     </div>
   </div>
 </template>
 
 <script>
+import pageTools from '@/components/pageTools'
 export default {
-
+  components: {
+    pageTools
+  }
 }
 </script>
 
